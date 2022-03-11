3404bf9cf0e8ce94628893da316717244f4965d0
Aramais Garibjan <aramo.g@hotmail.de>
2022-03-11 14:44:57 +0100

    /

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import styled from "styled-components";
+
 export default function App({ Component, pageProps }) {
+  const [players, setPlayers] = useState([]);
+  const [nameOfGame, setNameOfGame] = useState("");
+  const [history, setHistory] = useState([]);
   return (
-    <>
+    <AppLayout>
       <h1>Scorekeeper</h1>
       <Component {...pageProps} />
-      <h2>Create a new game</h2>
-    </>
+    </AppLayout>
   );
 }
+const AppLayout = styled.div`
+  display: grid;
+  gap: 20px;
+  padding: 20px;
+`;
